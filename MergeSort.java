49f651a3ad01cae452193d3cedc1665fc2e0c60b
Daniel <drodriguez11375@gmail.com>
2019-04-03 17:44:28 -0400

    Initial commit

---
+++ MergeSort.java
@@ -0,0 +1,85 @@
+import java.util.*;
+public class MergeSort
+{
+	// Public wrapper function
+	public static void mergeSort(int [] arr)
+	{
+		mergeSort(arr, 0, arr.length - 1);
+	}
+
+	// Actual recursive sorting function
+	private static void mergeSort(int [] arr, int lo, int hi)
+	{
+		// Base Case
+		if (lo >= hi)
+			return;
+		
+		int mid = lo + (hi - lo) / 2;
+		// Recursive calls
+		mergeSort(arr, lo, mid);
+		mergeSort(arr, mid + 1, hi);
+
+		// The Merge operation (2 parts)
+		// A: Merge 2 subarrays into an auxiliary array
+		int [] aux = new int[hi - lo + 1];
+
+		int i = lo;
+		int j = mid + 1;
+		int k = 0;
+
+		// Walk both subarrays, merging back into the
+		// auxiliary 
+		while (i <= mid || j <= hi)
+		{
+			if (j > hi)
+				aux[k++] = arr[i++];
+			else if (i > mid)
+				aux[k++] = arr[j++];
+			else if (arr[i] < arr[j])
+				aux[k++] = arr[i++];
+			else
+				aux[k++] = arr[j++];
+		}
+
+		// B: Merge the auxiliary array back into main array
+		for (i = lo; i <= hi; i++)
+			arr[i] = aux[i - lo];
+	}
+
+	public static void printArr(int [] arr)
+	{
+		for (int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+	public static void main(String [] args)
+	{
+		int n = Integer.parseInt(args[0]);
+		if (args.length < 1)
+		{
+			System.out.println("Use an integer > 1");
+			return;
+		}
+		if (n < 1)
+		{
+			System.out.println("Use an integer > 1");
+			return;
+		}
+			
+
+
+		int [] arr = new int[n];
+
+		for (int i = 0; i < n; i++)
+			arr[i] = (int)(Math.random() * 100 + 1);
+
+		printArr(arr);
+	
+		mergeSort(arr);
+	
+		printArr(arr);
+
+		return;
+	}
+}
