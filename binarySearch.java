4ebcede153d27a953d15a89d62d802329b5f28f3
Daniel <drodriguez11375@gmail.com>
2019-04-11 16:48:09 -0400

    Added an iterative implementation

--- binarySearch.java
+++ binarySearch.java
@@ -1,15 +1,37 @@
 import java.util.*;
 
 // Daniel Rodriguez
-// Returns the index of key if it exists in the array, otherwise
-// returns -1
+// Returns the index of key if it exists in the (already sorted) array, 
+// otherwise returns -1.
+// 
+// There are 2 implementations, one iterative, one recursive.
+
 public class binarySearch
 {
 	public static int binarySearch(int [] arr, int key)
 	{
-		return binarySearch(arr, key, 0, arr.length - 1);
+		int lo = 0, hi = arr.length -1;
+		int mid;
+
+		while (lo < hi)
+		{
+			mid = lo + (hi - lo) / 2;
+
+			if (arr[mid] > key)
+				hi = mid - 1;
+			else if (arr[mid] < key)
+				lo = mid + 1;
+			else if (arr[mid] == key)
+				return mid;
+		}
+		return -1;
 	}
-	private static int binarySearch(int [] arr, int key, int lo,
+
+	public static int binarySearchRecursive(int [] arr, int key)
+	{
+		return binarySearchRecursive(arr, key, 0, arr.length - 1);
+	}
+	private static int binarySearchRecursive(int [] arr, int key, int lo,
 	                                 int hi)
 	{
 		// Base case
@@ -21,18 +43,19 @@ public class binarySearch
 		if (key == arr[mid])
 			return mid;
 		else if (key < arr[mid])
-			return binarySearch(arr, key, lo, mid - 1);
+			return binarySearchRecursive(arr, key, lo, mid - 1);
 		else 
-			return binarySearch(arr, key, mid + 1, hi);
+			return binarySearchRecursive(arr, key, mid + 1, hi);
 
 	}
 
 	// Driver code
 	public static void main(String [] args)
 	{
-		int [] arr = {2,3,7,0,8};
+		int [] arr = new int[] {1,2,3,4,5};
 
 		System.out.println(binarySearch(arr, 3));
+		System.out.println(binarySearchRecursive(arr,3));
 
 		return;
 	}
